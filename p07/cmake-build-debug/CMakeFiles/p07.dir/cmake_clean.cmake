d7a4de45b66b97a58973e35d974bb62b63c6c082
Jviscenheski <jviscenheski@alunos.utfpr.edu.br>
2019-10-16 00:03:10 -0300

    começando o p07

---
+++ p07/cmake-build-debug/CMakeFiles/p07.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p07.dir/pingpong-maintask.c.o"
+  "CMakeFiles/p07.dir/pingpong.c.o"
+  "CMakeFiles/p07.dir/queue.c.o"
+  "p07.pdb"
+  "p07.exe"
+  "libp07.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/p07.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
